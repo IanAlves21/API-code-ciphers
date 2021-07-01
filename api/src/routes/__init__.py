4c2beec3d78d022612cb1c013281e61cfdb2497e
Ian Alves <ian.alves21@gmail.com>
2021-06-30 21:32:08 -0400

    Add IDEA endopint do encrypt and decrypt message

--- api/src/routes/__init__.py
+++ api/src/routes/__init__.py
@@ -3,5 +3,6 @@ from .route_example import example
 from .AES import aes
 from .DES import des
 from .DES3 import des3
+from .IDEA import idea
 
-routes = Blueprint.group([example, aes, des, des3])
+routes = Blueprint.group([example, aes, des, des3, idea])
