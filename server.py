c417731f21d46224a2f2b0b5122492bcf5b46056
Ian Alves <ian.alves21@gmail.com>
2021-07-24 11:03:52 -0400

    update to heroku

--- server.py
+++ server.py
@@ -1,29 +1,29 @@
-from dotenv.main import load_dotenv
+# from dotenv.main import load_dotenv
 from pathlib import Path
 from src.app import app
 
 import os
 
 path_env = Path('.') / '.env'
-load_dotenv(dotenv_path=path_env)
+# load_dotenv(dotenv_path=path_env)
 
 if __name__ == '__main__':
-    if os.environ['APP'] == 'production':
-        app.run(
-            host=os.environ['APP_HOST'],
-            port=int(os.environ['APP_PORT']),
-            debug=False,
-            access_log=False,
-            auto_reload=False,
-            workers=int(os.environ['APP_WORKERS'])
-        )
+    # if os.environ['APP'] == 'production':
+    app.run(
+        host="localhost",
+        port=int("80"),
+        debug=False,
+        access_log=False,
+        auto_reload=False,
+        workers=int("4")
+    )
 
-    elif os.environ['APP'] == 'development':
-        app.run(
-            host=os.environ['APP_HOST'],
-            port=int(os.environ['APP_PORT']),
-            debug=False if os.environ['APP_DEBUG'] == 'false' else True,
-            access_log=False if os.environ['APP_DEBUG'] == 'false' else True,
-            workers=int(os.environ['APP_WORKERS']),
-            auto_reload=True
-        )
+    # elif os.environ['APP'] == 'development':
+    #     app.run(
+    #         host=os.environ['APP_HOST'],
+    #         port=int(os.environ['APP_PORT']),
+    #         debug=False if os.environ['APP_DEBUG'] == 'false' else True,
+    #         access_log=False if os.environ['APP_DEBUG'] == 'false' else True,
+    #         workers=int(os.environ['APP_WORKERS']),
+    #         auto_reload=True
+    #     )
