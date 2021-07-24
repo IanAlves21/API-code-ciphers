d99eeb853829b29a86404d57355c6e96ca676efc
Ian Alves <ian.alves21@gmail.com>
2021-07-24 11:51:10 -0400

    update to heroku

--- server.py
+++ server.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     # if os.environ['APP'] == 'production':
     app.run(
         host="0.0.0.0",
-        port=int("5000"),
+        port=int(os.environ['PORT']|"5000"),
         debug=False,
         access_log=False,
         auto_reload=False,
